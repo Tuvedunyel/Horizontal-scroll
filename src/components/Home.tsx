b8072b587e27d76b202b9c7e7d88e04d9a676cd4
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-05-25 17:25:28 +0200

    distort meshe

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,48 +1,35 @@
-import { FC } from 'react';
-import './style.css'
+import { FC, Suspense } from 'react';
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stars } from '@react-three/drei';
-import { Physics, useBox, usePlane } from '@react-three/cannon';
+import { OrbitControls } from '@react-three/drei';
+import Box from "./Box";
+import './style.css'
+import AnimatedSphere from "./AnimatesSphere";
 
-const Sphere = () => {
-    const [ref, api] = useBox( () => ( { mass: 0.2 } ) )
-    return (
-        <mesh onPointerEnter={() => {
-            api.velocity.set(0, 5, 0)
-        }} ref={ref} position={[0, 2, 0]}>
-            <circleBufferGeometry attach="geometry" args={[5, 50]}  />
-            <meshLambertMaterial attach="material" color="hotpink" />
-        </mesh>
-    )
-}
-
-const Plane = () => {
-    const [ref] = usePlane( () => ( {
-        rotation: [-Math.PI / 2, 0, 0],
-    } ) )
-    return (
-        <mesh ref={ref} position={[0, 0, 0]}>
-            <planeBufferGeometry attach="geometry" args={[100, 100]} />
-            <meshLambertMaterial attach="material" color="white" />
-        </mesh>
-    )
-}
 
 const Home: FC = () => {
 
-        return (
-            <Canvas>
-                <OrbitControls />
-                <Stars />
-                <ambientLight intensity={ 0.5 } />
-                <spotLight position={[10, 15, 10]} angle={0.3} />
-                <Physics>
-                    <Sphere />
-                    <Plane />
-                </Physics>
+    return (
+        <>
+            <h1>Hello</h1>
+            <Canvas className="canvas">
+                <OrbitControls enableZoom={ false }/>
+                <ambientLight intensity={ 0.5 }/>
+                <directionalLight position={ [ -2, 5, 2 ] } intensity={ 1 }/>
+                <Suspense fallback={ null }>
+                    <Box/>
+                </Suspense>
             </Canvas>
-        );
-    }
-;
+            <Canvas className="canvas">
+                <OrbitControls enableZoom={ false }/>
+                <ambientLight intensity={ 0.5 }/>
+                <directionalLight position={ [ -2, 5, 2 ] } intensity={ 1 }/>
+                <Suspense fallback={ null }>
+                    <AnimatedSphere />
+                </Suspense>
+            </Canvas>
+        </>
+    );
+}
 
 export default Home;
+
